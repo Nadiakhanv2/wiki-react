ca64c7d418fcb71e6371e292003696a6dbc73291
Nadia Iqbal <nadiaiqbal2600@example.com>
2022-06-30 14:40:03 +0500

    create wiki react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header";
+import Dice from "./components/dice";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
 
-function App() {
+export default function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState("null");
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === `light`) {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been anabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been anabled", "success");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+
+      
+      <hr />
+      <Dice />
+      <hr />
+      <TextForm heading="Enter the Text" showAlert={showAlert} />
     </div>
   );
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+      
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,25 @@
+
+export default function Header(props) {
+  return (
+    <header
+      className={`d-flex align-items-center px-2 py-2 header bg-${props.mode} text-${props.mode === `light`?`dark`:`light`}`}
+    >
+      <div className="col-4">
+        <img
+          src="https://www.logolynx.com/images/logolynx/64/6434117060654972bd19ca515db70158.jpeg"
+          alt="meme-logo"
+          className="header--image"
+          width={65}
+        />
+      </div>
+      <h1 className="header--title col-3 text-center">Meme Generator</h1>
+      <h2 className="header--project col-3 text-center">
+        react course-project
+      </h2>
+      <div className="form-check form-switch col-2">
+         <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onClick={props.toggleMode}/>
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+       </div> 
+    </header>
+  );
+}
